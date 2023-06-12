a559b7e7027459bf1c5b13fd7397be3efe42984d
dave-hickman <dave.robert.hickman@gmail.com>
2023-06-12 12:04:16 +0100

    create character component, add default movements, install packages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { Canvas } from "@react-three/fiber";
+import Scene from "./components/Scene";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="canvas-container">
+        <Canvas camera={{position: [10,10,10], fov: 50}}>
+          <Scene/>
+        </Canvas>
+      </div>
+    </>
   );
 }
 
