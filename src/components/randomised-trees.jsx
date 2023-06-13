fdb41fab373b5474589415966ad1c673c18c42a7
saimakhanom <saimakhanom@gmail.com>
2023-06-13 10:19:54 +0100

    memoize obstacle and tree positions

--- src/components/randomised-trees.jsx
+++ src/components/randomised-trees.jsx
@@ -1,12 +1,26 @@
+import { useMemo } from "react";
 import { generateRandomGrassPosition } from "../utils/random-grass-points";
 
-export default function RandomisedGrassComponents({ planeDimensions, Component, objectSize = 1, numObjects=10, buffer=10  }) {
+export default function RandomisedGrassComponents({
+  planeDimensions,
+  Component,
+  objectSize = 1,
+  numObjects = 10,
+  buffer = 10,
+}) {
   const { pathLength, groundWidth } = planeDimensions;
   const pathConstraint = pathLength / 2;
 
-  const objectPositions = Array.from({ length: numObjects }, () =>
-    generateRandomGrassPosition(buffer, objectSize, pathConstraint, groundWidth)
+  const objectPositions = useMemo(() => {
+    return Array.from({ length: numObjects }, () =>
+      generateRandomGrassPosition(
+        buffer,
+        objectSize,
+        pathConstraint,
+        groundWidth
+      )
     );
+  }, [buffer, objectSize, pathConstraint, groundWidth, numObjects]);
 
   return (
     <>
@@ -14,7 +28,7 @@ export default function RandomisedGrassComponents({ planeDimensions, Component, 
         if (position === undefined) {
           return null;
         }
-          return <Component key={index} position={position} scale={objectSize} /> 
+        return <Component key={index} position={position} scale={objectSize} />;
       })}
     </>
   );
