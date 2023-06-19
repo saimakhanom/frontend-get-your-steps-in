6ffc901a03a1f1f9be496744299b8570b2b7a769
dave-hickman <dave.robert.hickman@gmail.com>
2023-06-19 17:08:28 +0100

    change welcome message

--- src/components/Loading-Page.jsx
+++ src/components/Loading-Page.jsx
@@ -29,12 +29,15 @@ export const Page = ({ setForward, setScore }) => {
           <div className="instructions">
             <h2>Instructions </h2>
             <p>
-              You are playing as John, a lad hell-bent on getting his steps in,
+            Welcome to Get Your Steps In, a thrilling adventure where you'll embark on a journey to win the ultimate prize — a delicious kebab! But it won't be an easy task... To earn your savory reward, you'll need to get some serious steps in for the day. Avoid hitting the obstacles in your path or you'll lose your motivation! Remember, every step counts...
+            </p>
+            {/* Welcome to Get Your Steps In, the fitness game for the coach potato.
+              If your step count is high enough you will be rewarded with a visit to the kebab shop, but be careful of the obstacles in your way or you'll soon run out of motivation for the journey!  */}
+            {/* You are playing as John, a lad hell-bent on getting his steps in,
               on his journey he encounters a load of obstacles blocking his
               path. If he were to
               hit one of these obstacles it would decrease his motivation to
-              continue running, if he hits 3 obstacles he's off for a kebab...
-            </p>
+              continue running, if he hits 3 obstacles he's off for a kebab... */}
         </div>
           <h2 className="ctrl-title">Controls</h2>
           <p>
