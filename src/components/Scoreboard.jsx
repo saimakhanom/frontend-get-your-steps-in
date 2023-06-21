0ac40b85beef32550d516eecbf4cd06278cc7d54
dave-hickman <dave.robert.hickman@gmail.com>
2023-06-21 09:22:22 +0100

    change model on scoreboard

--- src/components/Scoreboard.jsx
+++ src/components/Scoreboard.jsx
@@ -1,93 +1,86 @@
 import { useNavigate } from "react-router-dom";
-import { useState } from "react";
-import { useLoader } from "@react-three/fiber";
+import { useRef, useState } from "react";
+import { useFrame, useLoader } from "@react-three/fiber";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
-import burgerFile from "../assets/Hamburger.glb"
+import kebabFile from "../assets/Kebab.glb";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
-
+import KebabScoreboard from "./Kebab-scoreboard";
 
 // mock data
 const highScorers = [
   {
     name: "Nadia",
-    score: 23456
+    score: 23456,
   },
   {
     name: "Dave",
-    score: 12456
+    score: 12456,
   },
   {
     name: "Joel",
-    score: 11942
+    score: 11942,
   },
   {
     name: "Saima",
-    score: 10456
+    score: 10456,
   },
   {
     name: "Jorge",
-    score: 16
-  }
-]
+    score: 16,
+  },
+];
 
 const Scoreboard = () => {
-  const [scorers] = useState(highScorers)
-  const navigate = useNavigate()
+  const [scorers] = useState(highScorers);
+  const navigate = useNavigate();
 
-  const model = useLoader(GLTFLoader, burgerFile);
-  
+  const model = useLoader(GLTFLoader, kebabFile);
 
   const goHome = () => {
-    navigate("/")
-  }
+    navigate("/");
+  };
 
   return (
     <div className="scoreboard-container">
-      
       <h1>Scoreboard</h1>
-      
+
       <div className="flex-wrapper">
-        
         <form className="score-form">
           <label htmlFor="userScore">Join the hall of fame: </label>
-          <input type="text" placeholder="Enter your name..." id="userScore"/>
+          <input type="text" placeholder="Enter your name..." id="userScore" />
           <button className="add-score">Join</button>
         </form>
 
         <div className="burger-canvas-container">
-        <Canvas className="burger-canvas">
-        <OrbitControls />
-        <ambientLight intensity={1}/>
-          <primitive
-            object={model.scene}
-            scale={0.27}
-            position={[0, -1, 0]}
-            rotation={[-0.1, 0, 0]}
-          />
+          <Canvas className="burger-canvas">
+            <OrbitControls />
+            <ambientLight intensity={1} />
+            <KebabScoreboard/>
           </Canvas>
-      </div>
-        
+        </div>
+
         <div className="board">
           <div className="board-header">
             <span>Name</span>
             <span>Steps</span>
           </div>
-          {scorers.map(score => {
+          {scorers.map((score) => {
             return (
               <div key={crypto.randomUUID()} className="score-item">
                 <span>{score.name}</span>
                 <span>{score.score}</span>
               </div>
-            )
+            );
           })}
         </div>
       </div>
 
-      <button className="playagain-btn" onClick={goHome}>Play again</button>
-    
+      <button className="playagain-btn" onClick={goHome}>
+        Play again
+      </button>
     </div>
   );
-}
- 
+};
+
 export default Scoreboard;
